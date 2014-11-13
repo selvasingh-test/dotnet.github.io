06fc398f556ac509f89161669aa4f7998b48d6ad
Peter Dave Hello <hsu@peterdavehello.org>
2014-11-13 11:38:07 +0800

    these resources should be loaded via protocol relative url

--- js/projects.js
+++ js/projects.js
@@ -1,8 +1,8 @@
 require.config({
     paths: {
-        "jquery": "http://code.jquery.com/jquery-2.1.1.min",
-        "marked": "http://cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min",
-        "typeahead" : "http://twitter.github.io/typeahead.js/releases/latest/typeahead.bundle"
+        "jquery": "//code.jquery.com/jquery-2.1.1.min",
+        "marked": "//cdnjs.cloudflare.com/ajax/libs/marked/0.3.2/marked.min",
+        "typeahead" : "//twitter.github.io/typeahead.js/releases/latest/typeahead.bundle"
     },
     shim: {
 		"typeahead": ["jquery"]
